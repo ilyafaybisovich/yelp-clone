79205a556cc95521a3ae71ed8467fb354891b55c
Joseph Knowles <joejknowles@gmail.com>
2015-05-06 11:14:21 +0100

    validates restaurant name uniqueness and rating being between 1 and 5

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,9 +2,16 @@ require 'spec_helper'
 
 describe Restaurant, type: :model do
   it { is_expected.to have_many(:reviews).dependent(:destroy) }
+
   it 'is not valid with a name of less than three characters' do
     restaurant = Restaurant.new(name: 'KF')
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
+  end
+
+  it 'is not valid unless it has a unique name' do
+    Restaurant.create(name: "Blakeman's burger")
+    restaurant = Restaurant.new(name: "Blakeman's burger")
+    expect(restaurant).to have(1).error_on(:name)
   end
 end
